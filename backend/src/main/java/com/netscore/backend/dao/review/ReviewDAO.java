ad4ed4c407fb1a86f3c95dc554d888fa8f175dda
bramlak <kalmarbalint97@gmail.com>
2025-09-12 17:37:42 +0200

    feat(dao): create review dao interface

--- backend/src/main/java/com/netscore/backend/dao/review/ReviewDAO.java
+++ backend/src/main/java/com/netscore/backend/dao/review/ReviewDAO.java
@@ -1,4 +1,13 @@
 package com.netscore.backend.dao.review;
 
+import com.netscore.backend.dao.model.review.NewReview;
+import com.netscore.backend.dao.model.review.Review;
+
+import java.util.List;
+
 public interface ReviewDAO {
+    List<Review> getAllReviewsByWebsiteId();
+    Review getReview(int id);
+    void createReview(NewReview newReview);
+    void deleteReview(int id);
 }
