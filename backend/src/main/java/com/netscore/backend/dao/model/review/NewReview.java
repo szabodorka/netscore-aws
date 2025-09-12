7733115edde44016bfbc097b7f989ef3a2b6ed77
bramlak <kalmarbalint97@gmail.com>
2025-09-12 18:55:00 +0200

    fix(model): change score type to int

--- backend/src/main/java/com/netscore/backend/dao/model/review/NewReview.java
+++ backend/src/main/java/com/netscore/backend/dao/model/review/NewReview.java
@@ -2,5 +2,5 @@ package com.netscore.backend.dao.model.review;
 
 import java.time.LocalDateTime;
 
-public record NewReview(String score, String comment, int userId, int websiteId) {
+public record NewReview(int score, String comment, int userId, int websiteId) {
 }
--- backend/src/main/java/com/netscore/backend/dao/model/review/Review.java
+++ backend/src/main/java/com/netscore/backend/dao/model/review/Review.java
@@ -2,5 +2,5 @@ package com.netscore.backend.dao.model.review;
 
 import java.time.LocalDateTime;
 
-public record Review(int id, String score, String comment, int userId, LocalDateTime postDate, int websiteId) {
+public record Review(int id, int score, String comment, int userId, LocalDateTime postDate, int websiteId) {
 }
