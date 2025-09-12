42a3feecd4fd751c200e631dfa99145c030459c6
bramlak <kalmarbalint97@gmail.com>
2025-09-12 18:56:22 +0200

    feat(service): implement review service methods

--- backend/src/main/java/com/netscore/backend/service/ReviewService.java
+++ backend/src/main/java/com/netscore/backend/service/ReviewService.java
@@ -1,4 +1,85 @@
 package com.netscore.backend.service;
 
+
+import com.netscore.backend.controller.dto.review.NewReviewDTO;
+import com.netscore.backend.controller.dto.review.ReviewDTO;
+import com.netscore.backend.dao.model.review.NewReview;
+import com.netscore.backend.dao.model.review.Review;
+import com.netscore.backend.dao.review.ReviewDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ReviewService {
+
+    private final ReviewDAO reviewDAO;
+
+    @Autowired
+    public ReviewService(ReviewDAO reviewDAO) {
+        this.reviewDAO = reviewDAO;
+    }
+
+    public List<ReviewDTO> getReviewsByWebsiteId(int id) {
+        try {
+            List<Review> reviews = reviewDAO.getAllReviewsByWebsiteId(id);
+            List<ReviewDTO> reviewDTOs = new ArrayList<>();
+            for (Review review : reviews) {
+                reviewDTOs.add(new ReviewDTO(
+                        review.id(),
+                        review.score(),
+                        review.comment(),
+                        review.userId(),
+                        review.postDate(),
+                        review.websiteId()
+                ));
+            }
+            return reviewDTOs;
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error while getting all reviews", e);
+        }
+    }
+
+    public ReviewDTO getReviewById(int id) {
+        try {
+            Review review = reviewDAO.getReview(id);
+            return new ReviewDTO(
+                    review.id(),
+                    review.score(),
+                    review.comment(),
+                    review.userId(),
+                    review.postDate(),
+                    review.websiteId()
+            );
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error while getting review by id", e);
+        }
+    }
+
+    public void deleteReviewById(int id) {
+        try {
+            reviewDAO.deleteReview(id);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error while deleting review", e);
+        }
+    }
+
+    public int addNewReview(NewReviewDTO newReviewDTO) {
+        try {
+            NewReview newReview = new NewReview(
+                    newReviewDTO.score(),
+                    newReviewDTO.comment(),
+                    newReviewDTO.userId(),
+                    newReviewDTO.websiteId()
+            );
+
+            return reviewDAO.createReview(newReview);
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Error while adding new review", e);
+        }
+    }
 }
+
+
