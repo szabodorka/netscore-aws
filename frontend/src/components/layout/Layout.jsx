c767f9ca9f7d9a0e698e2376eff071b85468b028
szabodorka <szabodorkam@gmail.com>
2025-09-25 17:55:47 +0200

    feat(navbar): implement Navbar with logo, Search input, Create review, My account and Logout buttons

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar/Navbar";
+import Navbar from "../Navbar/Navbar";
 import "./Layout.css";
 
 export default function Layout() {
   return (
     <div className="layout">
       <Navbar />
-      <main className="layout-main">
+      <main className="layout-container">
         <Outlet />
       </main>
     </div>
