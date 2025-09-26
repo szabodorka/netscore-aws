3d6aa848b3e029ac685af529716c2e0331b106b8
bramlak <kalmarbalint97@gmail.com>
2025-09-26 12:49:11 +0200

    feat(dao): create getAllReviewsByUserId interface

--- backend/src/main/java/com/netscore/backend/dao/review/ReviewDAO.java
+++ backend/src/main/java/com/netscore/backend/dao/review/ReviewDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface ReviewDAO {
     List<Review> getAllReviewsByWebsiteId(int id);
+    List<Review> getAllReviewsByUserId(int id);
     Review getReview(int id);
     int createReview(NewReview newReview);
     void deleteReview(int id);
