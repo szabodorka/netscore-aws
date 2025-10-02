ac21ea703f9ca1de26b9f7da6da69146f8733fd1
bramlak <kalmarbalint97@gmail.com>
2025-10-02 23:15:03 +0200

    feat(state): configure s3 bucket for state management

---
+++ terraform-backend/main.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "netscore-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
