2d6c8807a0a7d7af0d2467a109d4f9998c493b08
bramlak <kalmarbalint97@gmail.com>
2025-10-02 23:16:10 +0200

    feat(lock): configure dynamodb for state lock management

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -19,3 +19,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_e
     }
   }
 }
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "netscore-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
