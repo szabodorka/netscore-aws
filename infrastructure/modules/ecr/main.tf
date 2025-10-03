982c44801aa142e0315e113cd0fd262815f3377c
bramlak <kalmarbalint97@gmail.com>
2025-10-03 17:03:58 +0200

    feat(repository): add backend repository

---
+++ infrastructure/modules/ecr/main.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.project_name}-backend"
+  image_tag_mutability = "MUTABLE"
+  force_delete = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-backend"
+  }
+}
