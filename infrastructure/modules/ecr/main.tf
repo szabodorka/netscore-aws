8235ea137cb4b15fdcf4394cb65db914732af481
bramlak <kalmarbalint97@gmail.com>
2025-10-03 17:04:22 +0200

    feat(repository): add frontend repository

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -10,3 +10,17 @@ resource "aws_ecr_repository" "backend" {
     Name = "${var.project_name}-backend"
   }
 }
+
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "${var.project_name}-frontend"
+  image_tag_mutability = "MUTABLE"
+  force_delete = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-frontend"
+  }
+}
