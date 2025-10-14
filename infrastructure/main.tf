42e9eab2b3f89381d50b5d5055cc235c7f6f32ef
szabodorka <szabodorkam@gmail.com>
2025-10-14 13:10:38 +0200

    feat(infra): integrate VPC module in root configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,3 +2,9 @@ module "ecr" {
   source = "./modules/ecr"
   project_name = var.project_name
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+  project_name = var.project_name
+  region = var.region
+}
