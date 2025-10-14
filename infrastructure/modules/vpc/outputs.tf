d3b60a21ad02efc4ef660c0fc976242cdb51008c
szabodorka <szabodorkam@gmail.com>
2025-10-14 13:34:48 +0200

    feat(infra): extend VPC outputs with cidr blocks needed for routing in RDS module

--- infrastructure/modules/vpc/outputs.tf
+++ infrastructure/modules/vpc/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "vpc_cidr_block" {
+  value = aws_vpc.main.cidr_block
+}
+
 output "private_subnet_ids" {
   value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 }
@@ -12,4 +16,8 @@ output "public_subnet_ids" {
 
 output "db_subnet_ids" {
   value = [aws_subnet.db_a.id, aws_subnet.db_b.id, aws_subnet.db_c.id]
+}
+
+output "db_subnet_cidr_blocks" {
+  value = [aws_subnet.db_a.cidr_block, aws_subnet.db_b.cidr_block, aws_subnet.db_c.cidr_block]
 }
