f32658e8bddc9cc17a7efe3dc3fd428d40e915be
bramlak <kalmarbalint97@gmail.com>
2025-10-23 18:49:26 +0200

    feature(module): declare vpn module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,3 +8,14 @@ module "vpc" {
   project_name = var.project_name
   region = var.region
 }
+
+module "vpn" {
+  source = "./modules/vpn"
+  project_name = var.project_name
+  organization_name = var.project_name
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = {subnet_id: module.vpc.private_subnet_ids[0]}
+  nat_subnet_id = module.vpc.private_subnet_ids[0]
+  vpn_domain = "vpn.${var.project_name}.com"
+  clients = var.vpn_clients
+}
